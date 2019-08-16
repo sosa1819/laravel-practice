35f58e1fc8a49be6f85289b63e07ef119ff5c159
sosa1819 <chibanaoya001@gmail.com>
2019-08-16 13:48:49 +0900

    見た目を綺麗にした

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+
+<head>
+       <meta charset="utf-8">
+        <meta http-equiv="X-UA-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
         <title>Laravel Quickstart - Basic</title>
 
         <!-- CSSとJavaScript -->
+        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     </head>
 
     <body>
@@ -14,5 +22,8 @@
         </div>
 
         @yield('content')
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     </body>
 </html>
