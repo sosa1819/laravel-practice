f9d20ee5866e77e1e885a0cdc51b8ae555e4c4b2
sosa1819 <chibanaoya001@gmail.com>
2019-08-16 13:48:58 +0900

    見た目を綺麗にした

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -8,7 +8,6 @@
 
         <title>Laravel Quickstart - Basic</title>
 
-        <!-- CSSとJavaScript -->
         <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
